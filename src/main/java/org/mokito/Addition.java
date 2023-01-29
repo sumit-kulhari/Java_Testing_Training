b54ce7af38cf88524c0a542c8353a8e07acb873d
Sumit Kulhari <sk4213@srmist.edu.in>
2023-01-29 13:22:17 +0530

    Added addition class

--- src/main/java/org/mokito/Addition.java
+++ src/main/java/org/mokito/Addition.java
@@ -2,14 +2,12 @@ package org.mokito;
 
 public class Addition {
     AdditionService service;
-
      public Addition(AdditionService service){
         this.service = service;
      }
 
     public int addition(int a,int b){
         System.out.println("In addition service");
-
         return service.add(a,b);
     }
 }
