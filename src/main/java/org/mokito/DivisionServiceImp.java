ab79063976cdcdf9c8c4c0a132041a8a51678ce4
Sumit Kulhari <sk4213@srmist.edu.in>
2023-01-29 16:36:35 +0530

    Added comments

--- src/main/java/org/mokito/DivisionServiceImp.java
+++ src/main/java/org/mokito/DivisionServiceImp.java
@@ -1,6 +1,10 @@
 package org.mokito;
 
 public class DivisionServiceImp implements DivisionService{
+
+//    This class is called a stub class
+//    A stub is an object that looks like a genuine object but only has the methods
+//    required for testing
     @Override
     public int div(int a,int b){
         System.out.println("In division implementation");
