6f41ea591b7d02ec28626655201c63c27d8604e6
Sumit Kulhari <sk4213@srmist.edu.in>
2023-01-30 23:10:15 +0530

    Tested powerMockito

--- src/main/java/org/powermokito/PowerTesting.java
+++ src/main/java/org/powermokito/PowerTesting.java
@@ -1,4 +1,15 @@
 package org.powermokito;
 
 public class PowerTesting {
+    private String privateMessage() {
+        return "Hello World!";
+    }
+
+    public static String staticMessage() {
+        return "Hello World!";
+    }
+
+    public final String finalMessage() {
+        return "Hello World!";
+    }
 }
--- src/test/java/com/powermokito/TestingPower.java
+++ src/test/java/com/powermokito/TestingPower.java
@@ -1,21 +1,35 @@
 package com.powermokito;
 
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
-class UtilityTest{
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermokito.PowerTesting;
 
-}
+import static org.mockito.Mockito.when;
 
+@PrepareForTest( { PowerTesting.class })
 public class TestingPower {
 
-    public void testStatic(){
-        String call = "Hello Everyone";
-        String callexpectation = "Hello Everyone";
-//        PowerMockito.mockStatic(Utility.class);
+    @Test
+    public void mockStaticMethodTest() {
+        //Mock static methods
+        PowerMockito.mockStatic(PowerTesting.class);
 
+        //Set expectation
+        when(PowerTesting.staticMessage()).thenReturn("New Message from Mock!");
 
+        //invoke the method
+        String message = PowerTesting.staticMessage();
+
+        //Assert the stub response
+        Assert.assertEquals(message, "New Message from Mock!");
     }
 
-
-
 }
+
+
+
+
